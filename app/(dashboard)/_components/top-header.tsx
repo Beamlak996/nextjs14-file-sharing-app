11e9bc4cf641a17222acf6d070a8bddd4353a43f
Beamlak996 <beamlaktsegaye996@gmail.com>
2024-03-22 01:11:43 -0900

    Sidebar & Navbar done

---
+++ app/(dashboard)/_components/top-header.tsx
@@ -0,0 +1,13 @@
+import { UserButton } from "@clerk/nextjs"
+import { AlignJustify } from "lucide-react"
+import Image from "next/image"
+
+export const TopHeader = () => {
+    return (
+        <div className="flex p-5 border-b items-center justify-between md:justify-end" >
+            <AlignJustify className="md:hidden" />
+            <Image src="/logo.svg" alt="" width={150} height={100} className="md:hidden" />
+            <UserButton afterSignOutUrl="/" />
+        </div>
+    )
+}
--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,12 +1,14 @@
 import { Sidebar } from "./_components/sidebar"
+import { TopHeader } from "./_components/top-header";
 
 const DashboardLayout = ({children}: {children: React.ReactNode}) => {
   return (
     <>
-      <div className="h-full md:w-64 flex-col fixed inset-y-0 z-[50]">
+      <div className="h-full md:w-64 flex-col fixed inset-y-0 z-[50] hidden md:flex">
         <Sidebar />
       </div>
       <div className="md:ml-64" >
+        <TopHeader />
         {children}
       </div>
     </>
