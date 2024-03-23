bdee4de2545061922b58e09f4a768a580ac728c4
Beamlak996 <beamlaktsegaye996@gmail.com>
2024-03-23 00:47:05 -0900

    firebase config done

---
+++ firebase-config.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCM7vk7KDlgjL_GNI1lebI-VRLynGVnor4",
+  authDomain: "file-sharing-750e8.firebaseapp.com",
+  projectId: "file-sharing-750e8",
+  storageBucket: "file-sharing-750e8.appspot.com",
+  messagingSenderId: "489550095736",
+  appId: "1:489550095736:web:c58f9dcc1411e0a41d148d",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
